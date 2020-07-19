9f68c2bcc1ac0af130ee0d9f0ec78ec32e512840
VanamaSravya <60335751+VanamaSravya@users.noreply.github.com>
2020-07-19 21:06:04 +0530

    commit

---
+++ task6/src/test/java/com/epam/task6/Apps.java
@@ -0,0 +1,14 @@
+package com.epam.task6;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+
+public class Apps {
+	@Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+
+}
